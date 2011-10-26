7b79d027fb9726eb8261bd325c271a6244c3f6a2
Dan Tenenbaum <dtenenba@fhcrc.org>
2011-10-26 18:04:11 +0000

    onload cleanup, example change
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/tinesath1probe@1800 db2202da-8704-0410-b924-b125b3cd84ef

--- R/zzz.R
+++
@@ -1,9 +0,0 @@
-.onLoad <- function(lib, pkgname) {
-  require(AnnotationDbi)
-  
-  ## load the data
-  thepath = .path.package(pkgname)
-  where   =  as.environment(match(paste("package:", pkgname, sep = ""),search()))
-
-  data(list = pkgname, package = pkgname, envir = where)
-}
